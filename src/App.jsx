9d3149e63135a140016ef1224591aec43b5f6aff
easylearningbd <elebdsoft@gmail.com>
2024-08-04 02:07:19 +0600

    8 Component Structure & Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import { CssBaseline } from '@mui/material';
 import { BrowserRouter,Routes, Route } from 'react-router-dom'; 
 import Movies from './components/Movies/Movies';
 import Actors from './components/Actors/Actors';
+import NavBar from './components/NavBar/NavBar';
+import MovieInformation from './components/MovieInformation/MovieInformation';
+import Profile from './components/Profile/Profile';
 
 const App = () => {
     return (
         <div>
             <CssBaseline />
             <BrowserRouter>
+                <NavBar/>
+                <main>
+                  <div>
                 <Routes>
+                    <Route path='/movie/:id' element={<MovieInformation/>} />
                     <Route path='/' element={<Movies/>} /> 
-                    <Route path='/actors/:id' element={<Actors/>} /> 
+                    <Route path='/actors/:id' element={<Actors/>} />
+                    <Route path='/profile/:id' element={<Profile/>} /> 
                 </Routes>
+                </div>
+                </main>
             </BrowserRouter>
             
         </div>
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieInformation = () => {
+    return (
+        <div>
+            Movie Information
+        </div>
+    );
+};
+
+export default MovieInformation;
