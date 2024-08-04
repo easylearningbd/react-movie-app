789ad4f8b01caa10eddafb43d8c00f8684f71459
easylearningbd <elebdsoft@gmail.com>
2024-08-05 00:39:20 +0600

    15 Sidebar Setup Part 3

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,6 +7,19 @@ import { Link } from 'react-router-dom';
 const redLogo = 'https://i.ibb.co/ft4skBS/logored.png';
 const blueLogo = 'https://i.ibb.co/5K6vdzx/logoblue.png';
 
+const categories = [
+    { label: 'Popular', value: 'popular' },
+    { label: 'Top Rated', value: 'top_rated' },
+    { label: 'Upcoming', value: 'upcoming' }
+]
+
+const demoCategories = [
+    { label: 'Comedy', value: 'comedy' },
+    { label: 'Action', value: 'action' },
+    { label: 'Horror', value: 'horror' },
+    { label: 'Animation', value: 'animation' }
+]
+
 const Sidebar = ({setMobileOpen}) => {
     const theme = useTheme();
     const classes = useStyles();
@@ -17,6 +30,24 @@ const Sidebar = ({setMobileOpen}) => {
           className={classes.image}
           src={theme.palette.mode === 'light' ? blueLogo : redLogo} alt="EasyLogo" /> 
           </Link>
+
+          <Divider/>
+
+        <List>
+            <ListSubheader>Categories </ListSubheader>
+            {demoCategories.map(({ label, value }) => (
+                <Link key={value} className={classes.links} to="/">
+                    <ListItem onClick={() => {}} button>
+                        <ListItemIcon>
+                            <img src={redLogo} className={classes.genreImage} height={30} />
+                        </ListItemIcon>
+                        <ListItemText primary={label} />
+                    </ListItem>
+                
+                </Link>
+            ) )}
+       </List>  
+
         </>
     );
 };
--- src/components/Sidebar/sidestyles.js
+++ src/components/Sidebar/sidestyles.js
@@ -3,9 +3,14 @@ import { makeStyles } from "@mui/styles";
 const drawerWidth = 240;
 
 export default makeStyles((theme) => ({
-    toolbar:{
-        
+    imageLink:{
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '10% 0'
     },
+    image: {
+        width: '70%'
+    }
    
     
 }));
