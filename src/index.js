3096d5ae9c427a3a699748516a1a55eed4f1f058
easylearningbd <elebdsoft@gmail.com>
2024-08-05 02:35:23 +0600

    18 Redux Setup Part 1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
   
 const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
     <ThemeProvider theme={theme}>
    <App/>
    </ThemeProvider>
+   </Provider>
   
 );
 
---
+++ src/services/TMDB.js
@@ -0,0 +1,7 @@
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+
+export const tmdbApi = createApi({
+
+});
