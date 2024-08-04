e89f0896e67179faee587eb24f1ae7c7eba5b954
easylearningbd <elebdsoft@gmail.com>
2024-08-05 03:17:51 +0600

    20 Redux Setup Part 3

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { tmdbApi } from "../services/TMDB";
 
 export default configureStore({
     reducer: {
-        
-    }
+        [tmdbApi.reducerPath]: tmdbApi.reducer,
+    },
+
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(tmdbApi.middleware),
 })
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
+
+    const { data } = useGetMoviesQuery();
+    console.log(data);
+
     return (
         <div>
             <h1>Movies Page </h1>
