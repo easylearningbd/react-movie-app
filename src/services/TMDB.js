13cb193c1cdb61cc38113bf75392f00a4f37e337
easylearningbd <elebdsoft@gmail.com>
2024-08-05 02:58:05 +0600

    19 Redux Setup Part 2

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,15 @@
-import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
 
 export const tmdbApi = createApi({
-
+    reducerPath: 'tmdbApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3'}), endpoints: (builder) => ({
+        getMovies: builder.query({
+            query: () => `/movie/popular?page=${page}&api_key=${tmdbApiKey}`
+        }),
+    }),
 });
+
+export const { useGetMoviesQuery } = tmdbApi;
