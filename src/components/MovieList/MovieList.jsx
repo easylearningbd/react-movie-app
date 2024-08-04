8c49094cf58bc790365a1376f3545a0ce83f6669
easylearningbd <elebdsoft@gmail.com>
2024-08-05 03:42:17 +0600

    21 Rendering Movies Part 1

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieList = () => {
+    return (
+        <div>
+            Move List
+        </div>
+    );
+};
+
+export default MovieList;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import MovieList from '../MovieList/MovieList';
 
 const Movies = () => {
 
-    const { data } = useGetMoviesQuery();
-    console.log(data);
+    const { data, error, isFetching } = useGetMoviesQuery();
+    // console.log(data);
+
+    if (isFetching) {
+        return ( 
+            <Box display="flex" justifyContent="center">
+                <CircularProgress size="4rem" />
+            </Box>
+        );
+    }
+
+    if (!data.results.length) {
+        return (
+            <Box display="flex" alignItems="center" mt="20px">
+                <Typography variant='h4'>
+            No Moveis That Match the name <br />
+            Please search for somthing ele 
+                </Typography>
+            </Box>
+        );
+    }
+
+    if (error) return 'An Error Has Occured.' 
 
     return (
         <div>
-            <h1>Movies Page </h1>
+             <MovieList movies={data} />
         </div>
     );
 };
