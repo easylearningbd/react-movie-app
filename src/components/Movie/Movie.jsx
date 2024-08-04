7683c898030d9eed93a3196c094d643b78aec8f2
easylearningbd <elebdsoft@gmail.com>
2024-08-05 04:49:56 +0600

    23 Rendering Movies Part 3

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Typography, Grid, Grow, Tooltip,Rating } from '@mui/material';
+import useStyles from './moviestyles'
+import { Link } from 'react-router-dom';
 
-const Movie = () => {
+const Movie = ({ movie, i }) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            movie
-        </div>
+         <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+            <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+        <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://i.ibb.co/ft4skBS/logored.png' } alt={movie.title} />
+        <Typography className={classes.title}>{movie.title}</Typography>
+        </Link>
+            </Grow>
+
+         </Grid>
     );
 };
 
