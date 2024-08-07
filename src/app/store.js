d764abff47394d944de7465922f4016583ca34ac
easylearningbd <elebdsoft@gmail.com>
2024-08-08 00:13:41 +0600

    28 Redux Slice Part 1

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from "../services/TMDB";
+import { genreOrCategory } from "../features/currentGenreOrCategory";
 
 export default configureStore({
     reducer: {
         [tmdbApi.reducerPath]: tmdbApi.reducer,
+        currentGenreOrCategory: genreOrCategory,
     },
 
     middleware: (getDefaultMiddleware) => 
