4bed1d8451b8abe68bdd237dbc5fb7b9c400779c
easylearningbd <elebdsoft@gmail.com>
2024-08-08 01:29:25 +0600

    31 Switch Genre Functionality Part 2

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -12,12 +12,27 @@ export const tmdbApi = createApi({
             query: () => `genre/movie/list?api_key=${tmdbApiKey}`
         }),
        
-        // Get Movies By Type
-        getMovies: builder.query({
-            query: () => `/movie/popular?page=${page}&api_key=${tmdbApiKey}`
-        }),
+    // Get Movies By Type
+    getMovies: builder.query({
+        query: ({genreIdOrCategoryName,page}) => {
+     
+            // Get Movies BY Category
+            if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+                return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+            }
+
+            // Get Movies BY genre
+            if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+                return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+            }
+            // Get Populer Movies
+            return `/movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+
+        }
+    }),
 
     }),
 });
 
 export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
+
