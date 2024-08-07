458350a6c398f5e07a899c836538f6eba4a70273
easylearningbd <elebdsoft@gmail.com>
2024-08-08 02:36:40 +0600

    33 Search Functionality Part 2

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,15 +3,24 @@ import { TextField, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import useStyles from './searchstyles';
+import { searchMovie } from '../../features/currentGenreOrCategory';
+import { useLocation } from 'react-router-dom';
 
 
 const Search = () => {
     const [query, setQuery] = useState('');
     const classes = useStyles();
-
-    const handleKeyPress = () => {
-
+    const dispatch = useDispatch();
+    const location = useLocation();
+     
+    
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            dispatch(searchMovie(query));
+        }
     };
+
+    if (location.pathname !== '/') return null;  
 
     return (
         <div className={classes.searchContainer}>
--- src/features/currentGenreOrCategory.js
+++ src/features/currentGenreOrCategory.js
@@ -11,8 +11,12 @@ export const genreOrCategory = createSlice({
         selectGenreOrCategory: (state, action) => {
             // console.log(action.payload)
              state.genreIdOrCategoryName = action.payload;
+             state.searchQuery = '';
+        },
+        searchMovie: (state,action) => {
+            state.searchQuery = action.payload;
         }
     }
 });
-export const { selectGenreOrCategory } = genreOrCategory.actions;
+export const { selectGenreOrCategory,searchMovie } = genreOrCategory.actions;
 export default genreOrCategory.reducer;
