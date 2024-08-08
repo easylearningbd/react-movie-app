54b5a7f55f2dbc8795a2001627ed4e1d115140d9
easylearningbd <elebdsoft@gmail.com>
2024-08-09 04:22:10 +0600

    43 Movie information page Part 3

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -26,11 +26,38 @@ const MovieInformation = () => {
     }
 
     return (
-        <Grid container className={classes.containerSpaceAround}>
-            <Grid item sm={12} lg={4} style={{ display: 'flex', marginBottom: '30px' }} >
-        <img className={classes.poster} src={`https://image.tmdb.org/t/p/w500/${data?.poster_path }`} alt="{ data?.title }" />
-            </Grid>
+    <Grid container className={classes.containerSpaceAround}>
+        <Grid item sm={12} lg={4} style={{ display: 'flex', marginBottom: '30px' }} >
+    <img className={classes.poster} src={`https://image.tmdb.org/t/p/w500/${data?.poster_path }`} alt="{ data?.title }" />
         </Grid>
+    <Grid item container direction='column' lg={7}>
+        <Typography variant='h4' align='center' gutterBottom>
+            {data?.title} ({data.release_date.split('-')[0] })
+        </Typography>
+        <Typography variant='h5' align='center' gutterBottom>
+            {data?.tagline} 
+        </Typography>
+
+    <Grid item className={classes.containerSpaceAround}>
+        <Box display='flex' align="center">
+         <Rating readOnly value={data.vote_average / 2 } />
+         <Typography variant='subtitle1' gutterBottom style={{ marginLeft: '10px'  }}>
+            {data?.vote_average} / 10
+         </Typography>
+        </Box>
+        <Typography align='center' gutterBottom style={{ fontSize: '16px'  }}>
+        {data?.runtime}min | Language: {data?.spoken_languages[0].name}
+        </Typography>
+    
+    </Grid>    
+
+
+    </Grid>    
+
+
+
+
+    </Grid>
     );
 };
 
