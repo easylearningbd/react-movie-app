48510cc0b2c047bcfe298192c6ff09e9f04a7d8a
easylearningbd <elebdsoft@gmail.com>
2024-08-09 01:18:08 +0600

    39 Authentication Logout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Typography,Button,Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
+
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = '/';
+    }
+
+
     return (
-        <div>
-            User Profile
-        </div>
+    <Box>
+        <Box display="flex" justifyContent="space-between" >
+            <Typography variant='h6' gutterBottom> My Profile </Typography>
+            <Button color='inherit' onClick={logout}>
+               Logout &nbsp; <ExitToApp/>
+            </Button>
+        </Box>
+    
+    </Box>
     );
 };
 
