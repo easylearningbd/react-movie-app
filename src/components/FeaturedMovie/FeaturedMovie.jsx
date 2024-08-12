9e19e552687c8c97e87b17b39acc28524e8e17bf
easylearningbd <elebdsoft@gmail.com>
2024-08-13 01:28:18 +0600

    66 Featured Movie Card Part 2

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -11,17 +11,23 @@ const FeaturedMovie = ({movie}) => {
 
     return (
         <Box component={Link} to={`/movie/${movie.id}`} className={classes.featuredCardContainer}>
-            <Card className={classes.card} classes={{ root:classes.cardRoot }}>
-                <CardMedia
-                media="picture"
-                alt={movie.title}
-                image={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-                title={movie.title}
-                className={classes.cardMedia}
-                >
-                    {movie.title}
-                </CardMedia>
-            </Card> 
+    <Card className={classes.card} classes={{ root:classes.cardRoot }}>
+        <CardMedia
+        media="picture"
+        alt={movie.title}
+        image={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+        title={movie.title}
+        className={classes.cardMedia}
+        >
+            <Box padding='20px'>
+        <CardContent className={classes.cardContent} classes={{ root:classes.cardContentRoot }}>
+        <Typography variant='h5' gutterBottom>{ movie.title }</Typography>
+        <Typography variant='body2' gutterBottom>{ movie.overview }</Typography>
+        </CardContent>
+            </Box>
+                
+        </CardMedia>
+    </Card> 
 
         </Box>
     );
