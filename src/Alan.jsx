8e8f27f1e7228bf692ecdd9e273a8f5854188dc1
easylearningbd <elebdsoft@gmail.com>
2024-08-13 03:54:34 +0600

    69 Alan AI - Part 2

---
+++ src/Alan.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useContext } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ColorModeContext } from "./utils/ToggleColorMode";
+
+const useAlan = () => {
+
+    const { setMode } = useContext(ColorModeContext);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        alanBtn({
+            key: '8e0b083e795c924d0f062a79df525cba2e956eca572e1d8b807a3e2338fdd0dc/stage',
+            host: 'v1.alan.app',
+            onCommand: (command,mode) => {
+              if (command === 'changeMode') {
+                if (mode === 'light') {
+                    setMode('light')
+                } else{
+                    setMode('dark')
+                }
+              }
+            }
+        });
+      }, [setMode]);
+
+
+    return null;
+};
+
+export default useAlan;
