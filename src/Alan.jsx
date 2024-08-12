d7c80500080ffd42315b53d04b56ac063682a754
easylearningbd <elebdsoft@gmail.com>
2024-08-13 05:27:40 +0600

    72 Alan AI - Part 5

--- src/Alan.jsx
+++ src/Alan.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ColorModeContext } from "./utils/ToggleColorMode";
 import { fetchToken } from "./utils";
+import { selectGenreOrCategory } from "./features/currentGenreOrCategory";
 
 const useAlan = () => {
 
@@ -15,8 +16,19 @@ const useAlan = () => {
         alanBtn({
             key: '8e0b083e795c924d0f062a79df525cba2e956eca572e1d8b807a3e2338fdd0dc/stage',
             host: 'v1.alan.app',
-            onCommand: ({ command, mode}) => {
-              if (command === 'changeMode') {
+            onCommand: ({ command, mode, genres, genreOrCategory}) => { 
+            if (command === 'chooseGenre') {
+                const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+                if (foundGenre) {
+                    navigate('/') ;
+                    dispatch(selectGenreOrCategory(foundGenre.id));
+                }else{
+                    const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+                    navigate('/') ;
+                    dispatch(selectGenreOrCategory(category));
+                }
+            } else if (command === 'changeMode') {
                 if (mode === 'light') {
                     setMode('light');
                 } else{
